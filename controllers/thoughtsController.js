717a579cb56c13c9efa4659a9534df1c7159d111
sjastrow <skjastrow@gmail.com>
2024-05-27 16:37:35 -0400

    completed controller funciton to get a thought by id.

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -16,3 +16,23 @@ const thoughtController = {
         res.sendStatus(400);
       });
   },
+
+ // get one Thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: "No thought with this id!" });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
