9fec4021091010fa2494e5834fb53fb12019e745
sjastrow <skjastrow@gmail.com>
2024-05-28 16:23:06 -0400

    completed user controllers route to get one user by id.

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,3 +16,29 @@ const userController = {
         res.sendStatus(400);
       });
   },
+
+  // get one user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "No user found with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
