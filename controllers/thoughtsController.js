3258bdc9bc165c2dd63892d2c83c55c0210aa018
sjastrow <skjastrow@gmail.com>
2024-05-28 16:19:47 -0400

    completed controller route to delete individual thought.

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -76,3 +76,29 @@ const thoughtController = {
       })
       .catch((err) => res.json(err));
   },
+
+   // delete Thought
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: "No thought with this id!" });
+        }
+
+        // remove thought id from user's `thoughts` field
+        return User.findOneAndUpdate(
+          { thoughts: params.id },
+          { $pull: { thoughts: params.id } }, //$pull removes from an existing values that match a specified condition.
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "Thought created but no user with this id!" });
+        }
+        res.json({ message: "Thought successfully deleted!" });
+      })
+      .catch((err) => res.json(err));
+  },
