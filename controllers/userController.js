17da43fb9a9d52323a5c5aabfa6884975946aaaa
sjastrow <skjastrow@gmail.com>
2024-05-28 16:22:23 -0400

    completed user controller route to get all users.

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+  // get all users
+  getAllUser(req, res) {
+    User.find({})
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
