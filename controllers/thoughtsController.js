23abe4d060ff5515ac6bfc8379c6fe73f41a0479
sjastrow <skjastrow@gmail.com>
2024-05-28 16:20:46 -0400

    completed thought controller route to add a reaction to user's thought.

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -102,3 +102,20 @@ const thoughtController = {
       })
       .catch((err) => res.json(err));
   },
+
+   // add reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $addToSet: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought with this id" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
