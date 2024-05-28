49b6f2c3e8b85661d32fa206087d18f088553413
sjastrow <skjastrow@gmail.com>
2024-05-28 16:24:02 -0400

    created user controllers routes to creat a user and then update user by id.

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,3 +42,26 @@ const userController = {
         res.sendStatus(400);
       });
   },
+
+ // create user
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+
+  // update user by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
